dff0c65b9adc6028e7253e1a23597ff11db16f78
thieusumo <nguyenthieupro93@gmail.com>
2022-10-12 09:06:41 +0700

    add google tag manager

--- resources/views/fe/pages/master.blade.php
+++ resources/views/fe/pages/master.blade.php
@@ -11,6 +11,14 @@
     <link rel="icon" href="{{ asset('images/logo/favicon_32x32.png') }}" sixe="32x32">
     <link rel="icon" href="{{ asset('images/logo/favicon_192x192.png') }}" sixe="196x196">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5LW8X88');</script>
+    <!-- End Google Tag Manager -->
+
     @stack('meta_seo')
     
     @include('fe.lib._css')
@@ -20,6 +28,10 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LW8X88"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('fe.partials._header')
 
     @yield('content')
